d1580ac9a5b2f646f4b3f04af8c3efabecb29d0e
John <masterjohn553@gmail.com>
2024-10-03 11:59:45 +0300

    fixed bug with string in OsHandler, and added Consts.h file

---
+++ Stealth/Consts.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#include <string>
+
+namespace Consts {
+	std::string INJECTOR_64_EXE_FILE_PATH = ".\external\Injectors\dll_injector64.exe";
+	std::string FIND_WHAT_ACCESS_THIS_ADDRESS_DLL_FILE_PATH = ".\external\dlls\PutHardwareBpHookDll.dll";
+
+}
+
+
+#endif
